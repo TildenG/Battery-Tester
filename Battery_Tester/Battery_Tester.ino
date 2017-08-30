364323d7432bad0b60b904aaac59bdaa2f90c8cb
Tilden <tilden@live.com.au>
2017-08-30 11:26:01 +0930

    minor update

--- Battery_Tester/Battery_Tester.ino
+++ Battery_Tester/Battery_Tester.ino
@@ -1,3 +1,18 @@
+/*
+Copyright 2017, Tilden Groves.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 #pragma GCC optimize ("-O2")           // O0 none, O1 Moderate optimization, 02, Full optimization, O3, as O2 plus attempts to vectorize loops, Os Optimize space
 #include <ESP8266WiFi.h>
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-BatteryTester
+Battery Tester for Hackerspace Adelaide.
+This is a prototype and as such is not a complete project, this code is for educational purposes only use at your own risk !
